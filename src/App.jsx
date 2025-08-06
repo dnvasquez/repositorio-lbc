4e347ed60f7a4cff0a44c80f0ed2037ad15bc00f
David A. Vásquez Stuardo <dnvasque@gmail.com>
2025-08-06 09:07:34 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -52,12 +52,14 @@ const App = () => {
           <a href="https://ge6y46-david-vasquez.shinyapps.io/KWEN-Fauna-potencial/" target="_blank" rel="noopener noreferrer" className="sidebar-link">
             KWEN Fauna Potencial
           </a>
+          <div className="link-separator"></div>
           <a href="https://www.appsheet.com/start/b59a0549-b78b-4906-86b7-5f43823cd56c#appName=KWENflora-337919552-24-10-21&page=dashboard&table=Base+de+datos&view=Centro+de+descargas" target="_blank" rel="noopener noreferrer" className="sidebar-link">
             KWEN Flora
           </a>
           <a href="https://ge6y46-david-vasquez.shinyapps.io/KWEN-Cobertura-flora/" target="_blank" rel="noopener noreferrer" className="sidebar-link">
             KWEN Flora Cobertura
           </a>
+          <div className="link-separator"></div>
           <a href="https://www.appsheet.com/start/5a5af7fd-d911-4673-a85a-533ede97bc2d#appName=KWENsuelo-337919552&page=gallery&sort=%5B%7B%22Column%22%3A%22ID+MENU%22%2C%22Order%22%3A%22Ascending%22%7D%5D&table=Menu&view=Menu" target="_blank" rel="noopener noreferrer" className="sidebar-link">
             KWEN Suelo
           </a>
