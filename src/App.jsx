84ae9a578a1732c8f175190bf041fd8e408f048a
David A. Vásquez Stuardo <dnvasque@gmail.com>
2025-08-06 09:05:28 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -42,12 +42,24 @@ const App = () => {
         <div className="sidebar-separator"></div>
 
         <div className="sidebar-section">
-          <h2>Aplicaciones LBC</h2>
-          <a href="https://lbc-evaluacion-ambiental.netlify.app/" target="_blank" rel="noopener noreferrer" className="sidebar-link">
-            Evaluación Ambiental
+          <h2>Enlaces LBC</h2>
+          <a href="https://www.appsheet.com/start/82a7a1d0-aa58-4f81-988f-4f85eb9b4553#appName=KWENfauna-337919552-24-10-08&page=dashboard&table=DarwinCore&view=Centro+de+descargas" target="_blank" rel="noopener noreferrer" className="sidebar-link">
+            KWEN Fauna
           </a>
-          <a href="https://lbc-ley-humedales.netlify.app/" target="_blank" rel="noopener noreferrer" className="sidebar-link">
-            Ley de Humedales
+          <a href="https://ge6y46-david-vasquez.shinyapps.io/KWEN-Fauna-Abundancia/" target="_blank" rel="noopener noreferrer" className="sidebar-link">
+            KWEN Fauna Abundancia
+          </a>
+          <a href="https://ge6y46-david-vasquez.shinyapps.io/KWEN-Fauna-potencial/" target="_blank" rel="noopener noreferrer" className="sidebar-link">
+            KWEN Fauna Potencial
+          </a>
+          <a href="https://www.appsheet.com/start/b59a0549-b78b-4906-86b7-5f43823cd56c#appName=KWENflora-337919552-24-10-21&page=dashboard&table=Base+de+datos&view=Centro+de+descargas" target="_blank" rel="noopener noreferrer" className="sidebar-link">
+            KWEN Flora
+          </a>
+          <a href="https://ge6y46-david-vasquez.shinyapps.io/KWEN-Cobertura-flora/" target="_blank" rel="noopener noreferrer" className="sidebar-link">
+            KWEN Flora Cobertura
+          </a>
+          <a href="https://www.appsheet.com/start/5a5af7fd-d911-4673-a85a-533ede97bc2d#appName=KWENsuelo-337919552&page=gallery&sort=%5B%7B%22Column%22%3A%22ID+MENU%22%2C%22Order%22%3A%22Ascending%22%7D%5D&table=Menu&view=Menu" target="_blank" rel="noopener noreferrer" className="sidebar-link">
+            KWEN Suelo
           </a>
         </div>
       </aside>
